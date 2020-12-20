6fd0beb02cc6953a2ef468824f26f0ee50e80f5b
Mustofa <mustofa_sachi@hotmail.com>
2020-12-20 16:31:47 +0700

    Connecting to a database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const authRoute = require('./routes/auth');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log("connected to db!")
+);
+
+// Route Middleware
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log("Server Up and running"));
